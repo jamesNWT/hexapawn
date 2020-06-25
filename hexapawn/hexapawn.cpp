bb678283584b8799c0183ec7a00dc3554f43ded5
James Wilcox <50497892+jamesNWT@users.noreply.github.com>
2020-06-25 15:39:49 -0300

    added game loop and methods/class to support it. Methods not implemented yet

--- hexapawn/hexapawn.cpp
+++ hexapawn/hexapawn.cpp
@@ -2,11 +2,130 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+class Move {
+private:
+	array<int, 2> fromCoord;
+	array<int, 2> toCoord;
+
+public:
+	Move() {}
+
+	Move(array<int, 2> f, array<int, 2> t)
+		: fromCoord(f), toCoord(t)
+	{}
+
+	bool equals(Move m)
+	{
+		return (this->fromCoord == m.fromCoord) && (this->toCoord == m.toCoord);
+	}
+};
+
+void printRules();
+void playGame();
+
+void draw(int board[3][3]);
+vector<Move> calculateLegalMoves(bool isPlayerTurn, int board[3][3]);
+bool checkGameover(int board[3][3], vector<Move> legalMoves);
+Move handleInput(vector<Move> legalMoves);
+Move makeCPUMove(vector<Move> legalMoves);
+void update(int (&board)[3][3], Move move);
+
 
 int main()
 {
-    std::cout << "James' Hexapawn game.\nType \"r\" for rules and \"s\" to start a game";
+    cout << "James' Hexapawn game.\nType \"r\" for rules, \"s\" to start a game, and \"q\" to quit\n";
+	string input;
+	cin >> input;
+	
+	if (input.compare("r") == 0)
+	{
+		printRules();
+	}
+	else if (input.compare("s") == 0)
+	{
+		playGame();
+	}
+	else
+	{
+		cout << "Type \"r\" for rules, \"s\" to start a game, and \"q\" to quit\n";
+	}
 
+}
+
+void printRules()
+{
+	cout << "I'll write up the rules later";
+}
+
+void playGame()
+{
+	// The board will be represented with a 3x3 matrix.
+	// -1 corresponds to 'X' pieces, 0 to empty squares. and 1 to 'O' pieces.
+	// Trickily, row 0 in the matrix is actually row 3 on the board.
+	// row 1 in matrix is 2 on the board, and row 2 in the matrix is row 1 on the board.
+	int board[3][3] = { {-1, -1, -1}, {0, 0, 0}, {1, 1, 1} };
+
+	// Game loop
+	bool gameOver = false;
+	bool isPlayerTurn = true;
+	while (!gameOver)
+	{
+		draw(board);
+		
+		vector <Move> legalMoves;
+
+		legalMoves = calculateLegalMoves(isPlayerTurn, board);
+
+		gameOver = checkGameover(board, legalMoves);
+
+		Move move;
+		
+		if (isPlayerTurn)
+		{
+			move = handleInput(legalMoves);
+
+		}
+		else
+		{
+			move = makeCPUMove(legalMoves);
+		}
+		
+		update(board, move);
+	}
+}
+
+void draw(int board[3][3])
+{
+}
+
+vector<Move> calculateLegalMoves(bool isPlayerTurn, int board[3][3])
+{
+	return vector<Move>();
+}
+
+bool checkGameover(int board[3][3], vector<Move> legalMoves)
+{
+	return false;
+}
+
+Move handleInput(vector<Move> legalMoves)
+{
+	return Move();
+}
+
+Move makeCPUMove(vector<Move> legalMoves)
+{
+	return Move();
+}
+
+void update(int(&board)[3][3], Move move)
+{
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
